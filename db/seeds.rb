052ca1da655fc2d75954607b156c00eaa0ae2fdf
Marine Chollet <marine.chollet@gmail.com>
2018-11-15 20:42:22 +0800

    added cocktails DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'open-uri'
+
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 ingredients_serialized = open(url).read
 ingredients = JSON.parse(ingredients_serialized)
@@ -13,4 +15,12 @@ ingredients = JSON.parse(ingredients_serialized)
 30.times do |n|
   name = ingredients["drinks"][n].values
   Ingredient.create!(name: name)
+end
+
+cocktail_url = 'https://raw.githubusercontent.com/teijo/iba-cocktails/master/recipes.json'
+cocktails_names = open(cocktail_url).read
+cocktails = JSON.parse(cocktails_names)
+
+cocktails.first(15).each do |c|
+  Cocktail.create(name: c['name'])
 end
